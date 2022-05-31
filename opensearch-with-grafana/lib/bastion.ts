113f391d0081f2299b02cbdac3720192ab92af73
Yuta Imai <imai.factory@gmail.com>
2022-06-01 06:22:26 +0900

    Made it work

--- opensearch-with-grafana/lib/bastion.ts
+++ opensearch-with-grafana/lib/bastion.ts
@@ -1,13 +1,15 @@
-import { aws_ec2 } from "aws-cdk-lib";
+import { aws_ec2, aws_iam, CfnOutput } from "aws-cdk-lib";
 import { Vpc } from "aws-cdk-lib/aws-ec2";
 import { Construct } from "constructs";
 
 export interface BastionHostProps {
-  vpc: Vpc
+  vpc: Vpc,
+  keyPairName: string
 }
 
 export class BastionHost extends Construct {
 
+  public readonly checkpointInstanceId: string;
   public readonly bastionHostSecurityGroup: aws_ec2.SecurityGroup;
   
   constructor(scope: Construct, id: string, props: BastionHostProps) {
@@ -18,17 +20,29 @@ export class BastionHost extends Construct {
       allowAllOutbound: true,
     });
 
-    const host = new aws_ec2.BastionHostLinux(this, 'BastionHost', {
-      vpc:props.vpc,
-      blockDevices: [{
-        deviceName: 'EBSBastionHost',
-        volume: aws_ec2.BlockDeviceVolume.ebs(10, {
-          encrypted: true,
-        }),
-      }],
-      securityGroup
+    const role = new aws_iam.Role(this, 'ec2Role', {
+      assumedBy: new aws_iam.ServicePrincipal('ec2.amazonaws.com')
+    })
+
+    role.addManagedPolicy(aws_iam.ManagedPolicy.fromAwsManagedPolicyName('AmazonSSMManagedInstanceCore'));
+
+    // Use Latest Amazon Linux Image - CPU Type ARM64
+    const ami = new aws_ec2.AmazonLinuxImage({
+      generation: aws_ec2.AmazonLinuxGeneration.AMAZON_LINUX_2,
+      cpuType: aws_ec2.AmazonLinuxCpuType.ARM_64
     });
 
+    const checkpointInstance = new aws_ec2.Instance(this, 'checkpoint', {
+      vpc:props.vpc,
+      instanceType: aws_ec2.InstanceType.of(aws_ec2.InstanceClass.T4G, aws_ec2.InstanceSize.MICRO),
+      machineImage: ami,
+      securityGroup,
+      role,
+      keyName:props.keyPairName
+    });
+
+    this.checkpointInstanceId = checkpointInstance.instanceId;
     this.bastionHostSecurityGroup = securityGroup;
+
   }
 }
